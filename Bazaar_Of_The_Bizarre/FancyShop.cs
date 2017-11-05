38fde9b7e13dedf03685e6cc965f029989354c44
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-11-05 14:40:26 +0100

    Created base for all classes in current class diagram

---
+++ Bazaar_Of_The_Bizarre/FancyShop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazaar_Of_The_Bizarre {
+	class FancyShop : IShop {
+	}
+}
