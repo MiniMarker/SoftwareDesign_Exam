083346d02d77d999174d3b0296ae311df812f77b
Emma Rasmusson <rasemm16@student.westerdals.no>
2017-11-07 17:25:38 +0100

    Fyllde i lite från UML CLASSEN YAO

--- Bazaar_Of_The_Bizarre/StoreFacade/Store.cs
+++ Bazaar_Of_The_Bizarre/StoreFacade/Store.cs
@@ -1,10 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bazaar_Of_The_Bizarre {
-	class Store {
-	}
+	class Store
+    {
+        private Shop _shop { get; set; }
+        private BackRoom _backroom { get; set; }
+        private String _name { get; set; }
+        private int _quota { get; set; }
+        private List<Products> _products { get; set; }
+
+        public Store(String name)
+        {
+            this._name = name;
+        }
+
+        public String GetName()
+        {
+            return _name;
+        }
+
+        public void SetQuota(int Quota)
+        {
+            this._quota = Quota;
+        }
+
+        public Product recieveProductFromBackroom()
+        {
+
+        }
+
+        public Product ViewSoldProducts()
+        {
+
+        }
+
+        public Boolean CloseStore()
+        {
+            return true;
+        }
+
+        public void SellProduct(int Price, int SocialSecurityNumber)
+        {
+
+        }
+
+        private void CreateShop(String type)
+        {
+
+        }
+    }
 }
