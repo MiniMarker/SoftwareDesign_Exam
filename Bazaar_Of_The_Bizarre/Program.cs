74ef4487e167b5f36b48f6ffa4f2299f01d69848
Christian <cfmarker@gmail.com>
2017-11-08 11:29:15 +0100

    decorator changes

--- Bazaar_Of_The_Bizarre/Program.cs
+++ Bazaar_Of_The_Bizarre/Program.cs
@@ -9,29 +9,56 @@ namespace Bazaar_Of_The_Bizarre {
 	class Program {
         public static void Main(string[] args)
         {
+			var backroom = new Backroom();
+
+	        backroom.CreateProduct(20);
+
+
+	        /*
             IStatue statue = new Statue();
             IStatue arm1 = new ArmsDecorator(statue);
             IStatue arm2 = new ArmsDecorator(arm1);
             IStatue arm3 = new ArmsDecorator(arm2);
             IStatue arm4 = new ArmsDecorator(arm3);
+            IStatue arm5 = new ArmsDecorator(arm4);
+
+	        
+	        IStatue arm4hat1 = new HatDecorator(arm4);
+            
+	        IStatue arm4hat2 = new HatDecorator(arm4hat1);
+
+            IStatue arm5hat2 = new ArmsDecorator(arm4hat2);
+
+            IStatue arm5hat3 = new HatDecorator(arm5hat2);
+            
+			//IStatue arm4hat1 = new HatDecorator(arm4);
+			//IStatue arm4hat2 = new HatDecorator(arm4hat1);
 
 
-            Console.WriteLine(statue.GetDescription());
-            Console.WriteLine(statue.GetPrice());
+			Console.WriteLine(statue.GetDescription());
+            
 
             Console.WriteLine(arm1.GetDescription());
-            Console.WriteLine(arm1.GetPrice());
+            
 
             Console.WriteLine(arm2.GetDescription());
-            Console.WriteLine(arm2.GetPrice());
+	       
 
             Console.WriteLine(arm3.GetDescription());
-            Console.WriteLine(arm3.GetPrice());
 
             Console.WriteLine(arm4.GetDescription());
-            Console.WriteLine(arm4.GetPrice());
+            
+			Console.WriteLine(arm5.GetDescription());
 
-            Console.ReadKey();
+			
+			Console.WriteLine(arm4hat1.GetDescription());
+
+	        /*Console.WriteLine(arm5hat2.GetDescription());
+
+	        Console.WriteLine(arm5hat3.GetDescription());
+	        */
+
+			Console.ReadKey();
 
         }
     }
--- Bazaar_Of_The_Bizarre/StatueDecorator/HatDecorator.cs
+++ Bazaar_Of_The_Bizarre/StatueDecorator/HatDecorator.cs
@@ -6,9 +6,38 @@ using System.Threading.Tasks;
 
 namespace Bazaar_Of_The_Bizarre.statueDecorator {
 	class HatDecorator : StatueDecorator {
-		protected HatDecorator(IStatue originalStatue) : base(originalStatue)
+
+		private int numberOfHats = 0;
+
+		public HatDecorator(IStatue originalStatue) : base(originalStatue)
 		{
 
+		}
+
+		public override double GetPrice()
+		{
+			return base.GetPrice() + 3.50;
+		}
+
+		public override string GetDescription()
+		{
+			var description = base.GetDescription();
+
+			if (!description.Contains("hat(s)"))
+			{
+				numberOfHats++;
+				description = base.GetDescription() + " " + numberOfHats + " hat(s)";
+			}
+			else
+			{
+				numberOfHats++;
+			}
+			return description;
+		}
+		
+		public void addArm()
+		{
+			//TODO FIX THIS PROBLEM!
 		}
 	}
 }
