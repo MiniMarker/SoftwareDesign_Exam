1109a6172af75a586b51cd9f78d64c7fdccc2c21
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-11-08 11:45:03 +0100

    Added randomizing in price for products

--- Bazaar_Of_The_Bizarre/StoreFacade/Store.cs
+++ Bazaar_Of_The_Bizarre/StoreFacade/Store.cs
@@ -8,38 +8,51 @@ using Bazaar_Of_The_Bizarre.statueDecorator;
 using Bazaar_Of_The_Bizarre.StoreFacade.ShopFactory;
 
 namespace Bazaar_Of_The_Bizarre.StoreFacade {
-	class Store
-	{
+	class Store {
 		public IShop Shop;
-        public Backroom Backroom { get; set; }
-        public string Name { get; set; }
-        public int Quota { get; set; }
-	    private List<Statue> _productsForSale;
+		public Backroom Backroom { get; set; }
+		public string Name { get; set; }
+		public int Quota { get; set; }
+		private List<Statue> _productsForSale;
+		Random rnd = new Random();
 
-   
-        public Statue RecieveProductFromBackroom()
-        {
-	        return null;
-        }
+		public Statue RecieveProductFromBackroom() {
+			return null;
+		}
 
-        public Statue ViewSoldProducts()
-        {
-	        return null;
-        }
+		public Statue ViewSoldProducts() {
+			return null;
+		}
 
-        public Boolean CloseStore()
-        {
-            return true;
-        }
+		public Boolean CloseStore() {
+			return true;
+		}
 
-        public void SellProduct(int price, int socialSecurityNumber)
-        {
+		public void SellProduct(int price, int socialSecurityNumber) {
 
-        }
+		}
 
-        private void GetShop(string type)
-        {
-	        ShopFactory.ShopFactory.CreateShop(ShopType.CheapShop, "cheapshit", 20);
-        }
-    }
+		private void GetShop(string type)
+		{
+			ShopType randomShopType = ChooseRandomType();
+			int productPrice = rnd.Next(30);
+
+			if (randomShopType == ShopType.ExpensiveShop)
+			{
+				productPrice = rnd.Next(30,40);
+			}
+			ShopFactory.ShopFactory.CreateShop(randomShopType, productPrice);
+		}
+
+		private ShopType ChooseRandomType() {
+			int randomType = rnd.Next(2);
+
+			if(randomType == 0) {
+				return ShopType.CheapShop;
+			}
+
+			return ShopType.ExpensiveShop;
+		}
+
+	}
 }
