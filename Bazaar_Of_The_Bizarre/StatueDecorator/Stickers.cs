bde22bcc591eed5d25f93a981fabb61c68f4867d
Christian <cfmarker@gmail.com>
2017-11-10 16:32:04 +0100

    Temp fix for printing statue. Consider options for getting print to a statue instead.  Move all print code to statue including list??

--- Bazaar_Of_The_Bizarre/StatueDecorator/Stickers.cs
+++ Bazaar_Of_The_Bizarre/StatueDecorator/Stickers.cs
@@ -1,4 +1,11 @@
-﻿namespace Bazaar_Of_The_Bizarre.StatueDecorator {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazaar_Of_The_Bizarre.StatueDecorator
+{
 	enum Stickers
 	{
 		Mario,
--- Bazaar_Of_The_Bizarre/StoreFacade/Backroom.cs
+++ Bazaar_Of_The_Bizarre/StoreFacade/Backroom.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using Bazaar_Of_The_Bizarre.statueDecorator;
 using Bazaar_Of_The_Bizarre.StatueDecorator;
 
-namespace Bazaar_Of_The_Bizarre.StoreFacade {
-	class Backroom {
+namespace Bazaar_Of_The_Bizarre.StoreFacade
+{
+	class Backroom
+	{
+		private List<string> _stickerList;
+		private List<string> _jewelList;
+		private List<string> _colorList;
+
 		private IStatue _statue = new Statue();
 		private readonly Random _random = new Random();
 
-		public IStatue CreateProduct(int numberOfDecorations) {
-
-
-			for(var i = 0; i < numberOfDecorations; i++) {
+		public IStatue CreateProduct(int numberOfDecorations)
+		{
+			for (var i = 0; i < numberOfDecorations; i++)
+			{
 				var value = _random.Next(1, 4);
 
-				switch(value) {
+				switch (value)
+				{
 					case 1:
-						if(CanUseDecoration(_statue.GetDescription(), "jewel")) {
+						if (CanUseDecoration(_statue.GetDescription(), "jewel"))
+						{
 							IStatue jewelDecoratedStatue = new JewelDecorator(_statue);
 							_statue = jewelDecoratedStatue;
 							break;
@@ -24,7 +32,8 @@ namespace Bazaar_Of_The_Bizarre.StoreFacade {
 						i--;
 						break;
 					case 2:
-						if(CanUseDecoration(_statue.GetDescription(), "sticker")) {
+						if (CanUseDecoration(_statue.GetDescription(), "sticker"))
+						{
 							IStatue stickerDecoratedStatue = new StickerDecorator(_statue);
 							_statue = stickerDecoratedStatue;
 							break;
@@ -32,7 +41,8 @@ namespace Bazaar_Of_The_Bizarre.StoreFacade {
 						i--;
 						break;
 					case 3:
-						if(CanUseDecoration(_statue.GetDescription(), "color")) {
+						if (CanUseDecoration(_statue.GetDescription(), "color"))
+						{
 							IStatue colorDecoratedStatue = new ColorDecorator(_statue);
 							_statue = colorDecoratedStatue;
 							break;
@@ -41,14 +51,16 @@ namespace Bazaar_Of_The_Bizarre.StoreFacade {
 						break;
 				}
 			}
-			Console.WriteLine(_statue.GetDescription());
+			SortDescription(_statue.GetDescription());
+			Console.WriteLine(PrintStatueDesc());
 			return _statue;
 		}
 
-		public List<IStatue> CreateManyStatues(int numberOfStatuesToBeCreated) {
+		public List<IStatue> CreateManyStatues(int numberOfStatuesToBeCreated)
+		{
 			List<IStatue> staueList = new List<IStatue>();
 
-			for(var i = 0; i < numberOfStatuesToBeCreated; i++)
+			for (var i = 0; i < numberOfStatuesToBeCreated; i++)
 			{
 				var numberOfDecorations = _random.Next(5, 10);
 
@@ -58,16 +70,20 @@ namespace Bazaar_Of_The_Bizarre.StoreFacade {
 			return staueList;
 		}
 
-		private bool CanUseDecoration(string description, string decorationType) {
-			if(GetAmountOfUsedDecorationsOfType(description, decorationType) < GetAmountOfPossibleDecorations(decorationType)) {
+		private bool CanUseDecoration(string description, string decorationType)
+		{
+			if (GetAmountOfUsedDecorationsOfType(description, decorationType) < GetAmountOfPossibleDecorations(decorationType))
+			{
 				return true;
 			}
 			return false;
 		}
 
-		private int GetAmountOfPossibleDecorations(string decoration) {
+		private int GetAmountOfPossibleDecorations(string decoration)
+		{
 			var possibleDecorations = 0;
-			switch(decoration) {
+			switch (decoration)
+			{
 				case "sticker":
 					possibleDecorations = Enum.GetValues(typeof(Stickers)).Length;
 					break;
@@ -82,8 +98,8 @@ namespace Bazaar_Of_The_Bizarre.StoreFacade {
 		}
 
 
-
-		private int GetAmountOfUsedDecorationsOfType(string description, string decorationType) {
+		private int GetAmountOfUsedDecorationsOfType(string description, string decorationType)
+		{
 			var descriptionWords = description.Split();
 
 			var amountOfStickers = 0;
@@ -91,19 +107,24 @@ namespace Bazaar_Of_The_Bizarre.StoreFacade {
 			var amountOfColors = 0;
 
 
-			foreach(var word in descriptionWords) {
-				if(Enum.IsDefined(typeof(Stickers), word)) {
+			foreach (var word in descriptionWords)
+			{
+				if (Enum.IsDefined(typeof(Stickers), word))
+				{
 					amountOfStickers++;
 				}
-				if(Enum.IsDefined(typeof(Colors), word)) {
+				if (Enum.IsDefined(typeof(Colors), word))
+				{
 					amountOfColors++;
 				}
-				if(Enum.IsDefined(typeof(Jewels), word)) {
+				if (Enum.IsDefined(typeof(Jewels), word))
+				{
 					amountOfJewels++;
 				}
 			}
 
-			switch(decorationType.ToLower()) {
+			switch (decorationType.ToLower())
+			{
 				case "sticker":
 					return amountOfStickers;
 				case "color":
@@ -113,6 +134,59 @@ namespace Bazaar_Of_The_Bizarre.StoreFacade {
 				default:
 					return 0;
 			}
+		}
+
+		public void SortDescription(string description)
+		{
+			_stickerList = new List<string>();
+			_jewelList = new List<string>();
+			_colorList = new List<string>();
+
+			var descriptionWords = description.Split();
+
+			foreach (var word in descriptionWords)
+			{
+				if (Enum.IsDefined(typeof(Jewels), word))
+				{
+					_jewelList.Add(word);
+				}
+
+				if (Enum.IsDefined(typeof(Stickers), word))
+				{
+					_stickerList.Add(word);
+				}
+				if (Enum.IsDefined(typeof(Colors), word))
+				{
+					_colorList.Add(word);
+				}
+			}
+		}
+
+		private string PrintStatueDesc()
+		{
+			string colors = "";
+			string jewels = "";
+			string stickers = "";
+
+			foreach (var color in _colorList)
+			{
+				colors += color + ", ";
+			}
+
+			foreach (var jewel in _jewelList)
+			{
+				jewels += jewel + " ,";
+			}
+
+			foreach (var sticker in _stickerList)
+			{
+				stickers += sticker + ", ";
+			}
+			return String.Format(
+				"{0} Statue \n " +
+				"- 'Jewels': {1} \n " +
+				"- Stickers: {2}",
+				colors, jewels, stickers); 
 		}
 	}
 }
