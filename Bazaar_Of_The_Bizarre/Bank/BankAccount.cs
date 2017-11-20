46e323640e1490c43d517f3e91fe9ccfcc8b4181
Emma Rasmusson <rasemm16@student.westerdals.no>
2017-11-20 15:39:15 +0100

    Banklock

--- Bazaar_Of_The_Bizarre/Bank/BankAccount.cs
+++ Bazaar_Of_The_Bizarre/Bank/BankAccount.cs
@@ -7,9 +7,22 @@ using System.Threading.Tasks;
 namespace Bazaar_Of_The_Bizarre {
 	class BankAccount
 	{
+<<<<<<< Updated upstream
 	    public double Sum { get; private set; }
+=======
+<<<<<<< Updated upstream
+	    private double _sum { get; set; }
+>>>>>>> Stashed changes
 
 	    public BankAccount(double sum)
+=======
+	    private int _sum { get; set; }
+	    private readonly Object _lock = new Object();
+
+
+
+        public BankAccount(int sum)
+>>>>>>> Stashed changes
 	    {
 	        Sum = sum;
 	    }
@@ -17,6 +30,7 @@ namespace Bazaar_Of_The_Bizarre {
         // Withdrawal tries to subtract SumToWithdraw if there is enough funds.
         public bool Withdrawal(double SumToWithdraw)
 	    {
+<<<<<<< Updated upstream
             //If the sum is smaller or equal to current Sum, a withdrawal of sum is performed.
 	        if (Sum >= SumToWithdraw)
 	        {
@@ -25,10 +39,23 @@ namespace Bazaar_Of_The_Bizarre {
 	            return true;
 	        }
 	        else
+=======
+            //If the sum is smaller or equal to current _sum, a withdrawal of sum is performed
+	        lock (_lock)
+>>>>>>> Stashed changes
 	        {
-	            Console.WriteLine("Not enough funds. Withdrawal was cancelled.");
-	            return false;
-	        }
+	            if (_sum >= SumToWithdraw)
+	            {
+	                _sum -= SumToWithdraw;
+	                Console.WriteLine("Withdrawal of {0} from account has been made. Current balance of account is {1} kr.", SumToWithdraw, _sum);
+	                return true;
+	            }
+	            else
+	            {
+	                Console.WriteLine("Not enough funds. Withdrawal was cancelled.");
+	                return false;
+	            }
+            }
 	    }
 	}
 }
