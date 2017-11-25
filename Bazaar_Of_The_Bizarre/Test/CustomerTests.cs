16763e4400a3bed9a746b2b63f61504ca1152be7
Christian <cfmarker@gmail.com>
2017-11-25 13:04:50 +0100

    tests

--- Bazaar_Of_The_Bizarre/Test/CustomerTests.cs
+++ Bazaar_Of_The_Bizarre/Test/CustomerTests.cs
@@ -1,7 +1,17 @@
-﻿namespace Bazaar_Of_The_Bizarre.Test
+﻿using Bazaar_Of_The_Bizarre.controller;
+using NUnit.Framework;
+
+namespace Bazaar_Of_The_Bizarre.Test
 {
+	[TestFixture]
 	public class CustomerTests
 	{
-		
+		[Test]
+		public void CreateCustomersTest()
+		{
+			Client client = new Client(2);
+
+
+		}
 	}
 }
