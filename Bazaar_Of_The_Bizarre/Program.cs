ea4f8b3b1dc5b84fd7735b855fcf24079caff3fa
Emma Rasmusson <rasemm16@student.westerdals.no>
2017-11-25 13:54:15 +0100

    dd

--- Bazaar_Of_The_Bizarre/Program.cs
+++ Bazaar_Of_The_Bizarre/Program.cs
@@ -3,7 +3,6 @@ using Bazaar_Of_The_Bizarre.controller;
 
 namespace Bazaar_Of_The_Bizarre {
 	class Program {
-		//TODO refactor program
 		public static void Main(string[] args) {
 		    var client = new Client(21);
 		    client.RunGame();
