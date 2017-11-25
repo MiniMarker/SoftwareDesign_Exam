0596e44dcc33292b3c3055aad41543038abc72b8
Emma Rasmusson <rasemm16@student.westerdals.no>
2017-11-25 17:48:15 +0100

    I fixed alittle thig

--- Bazaar_Of_The_Bizarre/Bank/BankFlyweight/BankFactory.cs
+++ Bazaar_Of_The_Bizarre/Bank/BankFlyweight/BankFactory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Bazaar_Of_The_Bizarre.Bank.BankFlyweight {
 	class BankFactory {
-	    private static Dictionary<String, Bazaar_Of_The_Bizarre.Bank.BankFlyweight.Bank> BANKMAP = new Dictionary<string, Bazaar_Of_The_Bizarre.Bank.BankFlyweight.Bank>();
+	    private static Dictionary<String, Bazaar_Of_The_Bizarre.Bank.BankFlyweight.Bank> _bankMap = new Dictionary<string, Bazaar_Of_The_Bizarre.Bank.BankFlyweight.Bank>();
 
         /// <summary>
         ///  Creates new bank if it does not exist and returns it. Otherwise existing bank is returned.
@@ -12,11 +12,11 @@ namespace Bazaar_Of_The_Bizarre.Bank.BankFlyweight {
         /// <returns>Bank Returns bank</returns>
 	    public static Bazaar_Of_The_Bizarre.Bank.BankFlyweight.Bank GetBank(String name)
 	    {
-	        if (!BANKMAP.ContainsKey(name))
+	        if (!_bankMap.ContainsKey(name))
 	        {
-                BANKMAP.Add(name, new Bazaar_Of_The_Bizarre.Bank.BankFlyweight.Bank(name));
+                _bankMap.Add(name, new Bazaar_Of_The_Bizarre.Bank.BankFlyweight.Bank(name));
 	        }
-	        return BANKMAP[name];
+	        return _bankMap[name];
 	    }
 
         /// <summary>
@@ -24,7 +24,7 @@ namespace Bazaar_Of_The_Bizarre.Bank.BankFlyweight {
         /// </summary>
 	    public static void PrintBanks()
 	    {
-	        foreach (KeyValuePair<String, Bazaar_Of_The_Bizarre.Bank.BankFlyweight.Bank> Pair in BANKMAP)
+	        foreach (KeyValuePair<String, Bazaar_Of_The_Bizarre.Bank.BankFlyweight.Bank> Pair in _bankMap)
 	        {
 	            Console.WriteLine("Name = {0}, Bank = {1}", Pair.Key, Pair.Value);
 	        }
