2adc36471d4d42c44d6b89ce12af619824feea47
Emma Rasmusson <rasemm16@student.westerdals.no>
2017-11-25 15:43:47 +0100

    ddddd

--- Bazaar_Of_The_Bizarre/Controller/Client.cs
+++ Bazaar_Of_The_Bizarre/Controller/Client.cs
@@ -1,51 +1,57 @@
 ﻿using System;
+using System.Threading;
 using Bazaar_Of_The_Bizarre.Bank.BankFlyweight;
 using Bazaar_Of_The_Bizarre.Controller;
 
-namespace Bazaar_Of_The_Bizarre.controller {
-	class Client {
+namespace Bazaar_Of_The_Bizarre.controller
+{
+    class Client
+    {
         public static readonly Random Rnd = new Random();
-	    public static readonly PrintHandler PrintProduct = PrintHandler.GetInstance();
+        public static readonly PrintHandler PrintProduct = PrintHandler.GetInstance();
         private readonly Bank.BankFlyweight.Bank _bank;
-	    private readonly Bazaar _bazaar;
-	    private readonly ThreadHandler _threadHandler;
+        private readonly Bazaar _bazaar;
+        private readonly ThreadHandler _threadHandler;
 
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="amountOfCustomers"></param>
-		public Client(int amountOfCustomers)
+        public Client(int amountOfCustomers)
         {
             _bank = BankFactory.GetBank("DNB");
             _bazaar = new Bazaar();
             _threadHandler = new ThreadHandler(amountOfCustomers);
         }
 
-       /// <summary>
-       /// Runs the whole game and start threads
-       /// </summary>
-	    public void RunGame()
-	    {
-	        _threadHandler.StartAllStoresThreads(_bazaar);
+        /// <summary>
+        /// Runs the whole game and start threads
+        /// </summary>
+        public void RunGame()
+        {
+            _threadHandler.StartAllStoresThreads(_bazaar);
 
-	        if (_bazaar.IsBazarOpen())
-	        {
-	            _threadHandler.StartAllCustomerThreads(_bank, _bazaar);
-	            while (_bazaar.IsBazarOpen())
-	            {
-	            }
-	            EndOfDay();
-	            Console.WriteLine("Bazar is now closed.");
-	        }
-	    }
-       
+            if (_bazaar.IsBazarOpen())
+            {
+                _threadHandler.StartAllCustomerThreads(_bank, _bazaar);
+                while (_bazaar.IsBazarOpen())
+                {
+
+                }
+                EndOfDay();
+                Console.WriteLine("Bazar is now closed.");
+            }
+        }
+
         /// <summary>
         /// Prints out all sold of the day by stores.
         /// </summary>
-        private void EndOfDay() {
-			foreach(var store in _bazaar.GetStoreList()) {
-				store.ViewSoldProducts();
-			}
-		}
-	}
+        private void EndOfDay()
+        {
+            foreach (var store in _bazaar.GetStoreList())
+            {
+                store.ViewSoldProducts();
+            }
+        }
+    }
 }
